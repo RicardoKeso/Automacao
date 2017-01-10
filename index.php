ff57359e497941cc37d975731691d6505e058939
RicardoKeso <ricardo_keso@hotmail.com>
2017-01-10 10:22:07 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php header("Refresh:30");?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+        <title>. : rpi : .</title>
+	</head>
+
+    <script src="scripts/jquery-2.2.3.min.js"></script>
+	<link rel="stylesheet" href="estilos/style_default.css" type="text/css">
+    <script src="scripts/scripts.js"></script>
+
+	<body>
+		<div id="principal">
+			<div>
+				<table> 
+					<tr>
+						<td><div id="7"></div></td>
+						<td><div id="12"></div></td>
+					</tr>
+				</table>
+			</div>
+		</div>
+	</body>
+</html>
